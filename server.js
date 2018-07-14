a4219354eb5a193a88da11c3d76392cc77989bf7
Wulfsbach <etemelkos@gmail.com>
2018-07-14 11:24:21 -0400

    added

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require ("express");
+var path = require('path');
+var bodpar= require('body-parser');
+var routesHtml = require('./app/routing/htmlRoutes');
+var app= express();
+
+var port = process.env.port || 3707;
+
+
+app.use('/', routesHtml);
+app.listen(port,function(){
+    console.log("Server listening on:localhost:" + port);
+});
+
