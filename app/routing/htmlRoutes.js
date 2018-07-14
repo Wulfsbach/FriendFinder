3d5cc514ee292cd96ebecac7f8a362c234aeb486
Wulfsbach <etemelkos@gmail.com>
2018-07-14 11:39:15 -0400

    html routed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,6 +6,8 @@ router.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'../public','home.html'));
 });
 
-router.get('/survey,')
+router.get('/survey' , function(req,res){
+res.sendFile(path.join(__dirname,"../public", 'survey.html'));
+});
 
 module.exports = router;
